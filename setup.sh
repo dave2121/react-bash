ebd6a6ca7f085bbfafb870bb06572c6556c3a925
Your Name <you@example.com>
2022-09-29 18:06:03 +0000

    chnge

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
+OS_Package=yum
 #Update Debian
-sudo apt update
+sudo $OS_Package update
 #Installing Node.js with Apt Using a NodeSource PPA
 cd ~
 curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
 sudo bash /tmp/nodesource_setup.sh
-sudo apt install nodejs
+sudo $OS_Package install nodejs
 #Creating a React Project. Using EOF function to answer (Y) to -Ok to proceed? (y)-
 npx create-react-app digital-ocean-app <<-EOF
 yes
@@ -13,5 +15,3 @@ EOF
 cd digital-ocean-app
 #trigger the app to start
 npm start
-
-
